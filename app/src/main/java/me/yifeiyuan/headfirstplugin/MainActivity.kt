585dc7c0b72e313117fdb85507545400640d9a80
mingjue <zhening.czn@alibaba-inc.com>
2020-03-31 23:09:23 +0800

    update

--- app/src/main/java/me/yifeiyuan/headfirstplugin/MainActivity.kt
+++ app/src/main/java/me/yifeiyuan/headfirstplugin/MainActivity.kt
@@ -9,12 +9,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        loadPlugin()
+        try {
+            Class.forName("me.yifeiyuan.plugin_app.Test")
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+        try {
+            loadPlugin()
+            val testClass = Class.forName("me.yifeiyuan.plugin_app.Test")
+            testClass.getDeclaredMethod("helloPlugin").invoke(null)
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
     }
 
-
-    val path = ""
+    val path = "/sdcard/plugin_app-debug.apk"
 
     private fun loadPlugin() {
 
@@ -25,12 +34,24 @@ class MainActivity : AppCompatActivity() {
         val dexElementsFiled = dexPathListClass.getField("dexElements")
 
         val hostDexPathList = dexPathListField.get(classLoader)
-        val hostDexElements : Array<Any> = dexElementsFiled.get(hostDexPathList) as Array<Any>
+        val hostDexElements: Array<Any> = dexElementsFiled.get(hostDexPathList) as Array<Any>
 
+        //加载插件
         val pluginPathClassLoader = PathClassLoader(path, cacheDir.absolutePath, null)
+        //获取插件的 ClassLoader 里的 dexPathList
         val pluginDexPathList = dexPathListField.get(pluginPathClassLoader)
+        //获取插件的 dexPathList 里的 dexElements
         val pluginDexElements: Array<Any> = dexElementsFiled.get(pluginDexPathList) as Array<Any>
 
-        
+        //创建新的 DexElements[]，用来替换老的宿主 dexElements
+        val newDexElements: Array<Any> = java.lang.reflect.Array.newInstance(hostDexElements.javaClass, hostDexElements.size + pluginDexElements.size) as Array<Any>
+
+        //将原来宿主的 dexElements 复制到新创建的 dexElements
+        System.arraycopy(hostDexElements, 0, newDexElements, 0, hostDexElements.size)
+        //将插件的 dexElements 复制到新创建的 dexElements
+        System.arraycopy(pluginDexElements, 0, newDexElements, hostDexElements.size, pluginDexElements.size)
+
+        //将宿主的 DexPathList 里的 dexElements 替换为新的 dexElements，完成插件的类加载
+        dexElementsFiled.set(hostDexPathList, newDexElements)
     }
 }
---
+++ app/src/main/java/me/yifeiyuan/headfirstplugin/TeT.java
@@ -0,0 +1,12 @@
+package me.yifeiyuan.headfirstplugin;
+
+/**
+ * Created by 程序亦非猿 on 2020/3/31.
+ */
+class TeT {
+
+    public static void main(String[] args) {
+
+    }
+
+}
